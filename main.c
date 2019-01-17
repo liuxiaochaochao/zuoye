c6df1efdedc5f2bc1913e81ffdb098909a083d99
chao <chao@qq.com>
2019-01-17 19:48:19 +0800

    zuoye

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int index = 0;
+    printf("欢迎进入专场位置管理系统!\n");
+    while (1)
+    {
+        printf("1---添加车辆位置\n");
+        printf("2---删除车辆位置\n");
+        printf("3---显示全部车辆位置\n");
+        printf("4---返回离我的位置最近的车辆\n");
+        printf("5---退出\n");
+        printf("请选择相应功能\n");
+        int a;
+        scanf("%d", &a);
+
+        if (a == 1)
+        {
+        }
+        if (a == 2)
+        {
+        }
+        if (a == 3)
+        {
+        }
+        if (a == 4)
+        {
+        }
+        if (a == 5)
+        {
+            printf("应用已经退出\n");
+            break;
+        }
+    }
+
+        return 0;
+    }
